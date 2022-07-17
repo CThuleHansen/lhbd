271cc58bd94e2072582c8b76671b78df5141a4bd
Casper Thule <c.thulehansen@live.com>
2022-07-17 22:24:38 +0200

    0302 ex

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,36 @@
 module Main where
 
 main :: IO ()
-main = putStrLn (makeHtml "My page title" "My page content")
+main = putStrLn myhtml
+
+myhtml :: String
+myhtml =
+  makeHtml
+    "Hello title"
+    (h1_ "Hello, world!" <> p_ "P content")
+
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
 
 html_ :: [Char] -> [Char]
-html_ xs = "<html>" <> xs <> "</html>"
+html_ = el "html"
 
 body_ :: [Char] -> [Char]
-body_ xs = "<body>" <> xs <> "</body>"
+body_ = el "body"
 
 head_ :: [Char] -> [Char]
-head_ xs = "<head>" <> xs <> "</head>"
+head_ = el "head"
 
 title_ :: [Char] -> [Char]
-title_ xs = "<title>" <> xs <> "</title>"
+title_ = el "title"
 
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+makeHtml :: [Char] -> [Char] -> [Char]
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
